be9a4af3676abff20d4fed3fc84be80c99847fc1
Karrel E. Catid <karrelecatid@su.edu.ph>
2021-06-25 15:02:02 +0800

    init

---
+++ src/components/create-post.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="newsfeed">
+    <label>Title:</label>
+    <input type="text" v-model="tempSkill">
+    <button @click='addSkill'>Post</button>
+    <div v-for="skill in skills" :key="skill">
+      {{skill}}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+    tempSkill: '',
+    skills: [],
+
+   }
+  },
+  methods: {
+    addSkill() {  
+      if (!this.skills.includes(this.tempSkill)) {
+         this.skills.push(this.tempSkill),
+      }
+      this.tempSkill = ''
+    },
+  },
+});
+</script>
---
+++ src/views/Newsfeed.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="newsfeed">
+    <h1>This is an about page</h1>
+    <CreatePost />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import CreatePost from '../components/create-post.vue'
+
+export default defineComponent({
+  name: 'newsfeed',
+  components: { CreatePost },
+})
+</script>
